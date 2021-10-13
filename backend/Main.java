1d03ccec60145333e76419327e5b761b53bc8280
chyna cobbs <cobbsc@rpi.edu>
2021-10-13 17:08:13 -0400

    Frat class
    frat class

--- backend/Main.java
+++ backend/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Sorority my_sorority = new Sorority(args[0], Integer.parseInt(args[1]));
+        Frat my_frat = new Frat(args[0], Interger.parseInt(args[1]));
+        System.out.println("Franitery: "+ my_frat.getName());
+        System.out.println("Total: "+ my_frat.getSize()); 
         System.out.println("Sorority: " + my_sorority.getName());
         System.out.println("Total: " + my_sorority.getSize());
     }

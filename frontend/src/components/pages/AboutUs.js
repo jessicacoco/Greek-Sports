e0f887d5cead7558b5f7afb628121efcfd47c671
emmaskantze <emmaskantze2018@gmail.com>
2021-10-17 15:24:28 -0400

    Made 3 tabs for searches

---
+++ frontend/src/components/pages/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <>
+        <Row justify="center">
+            <Col span={24}>
+                <h1>Who We Are</h1>
+            </Col>
+        </Row>
+    </>
+  );
+}
+
+export default AboutUs;

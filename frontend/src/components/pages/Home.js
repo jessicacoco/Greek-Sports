2206bb182fba03cab8fc2c3da79c3903e31dcffc
emmaskantze <emmaskantze2018@gmail.com>
2021-10-18 17:21:19 -0400

    Created 1 SearchCard to deal with all 3 searches

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Tabs } from 'antd';
 import FSSearchCard from '../FSSearchCard';
 import SportSearchCard from '../SportSearchCard';
 import ClubSearchCard from '../ClubSearchCard';
+import SearchCard from '../SearchCard';
 import './Home.css';
 
 const { TabPane } = Tabs;
 
 function Home() {
+  const [results, setResults] = useState([]);
+
+  function setSearchResults(values) {
+      setResults(values);
+  }
+
   return (
     <>
         <Row justify="center">
@@ -19,13 +26,16 @@ function Home() {
             <Col span={16}>
                 <Tabs defaultActiveKey="1" centered>
                     <TabPane tab="Search by Fraternity/Sorority" key="1">
-                        <FSSearchCard/>
+                        <FSSearchCard />
+                        <SearchCard results={results} setSearchResults={setSearchResults} type={"greek"}/>
                     </TabPane>
                     <TabPane tab="Search by Varsity/Club Sport" key="2">
                         <SportSearchCard />
+                        <SearchCard results={results} type={"sports"}/>
                     </TabPane>
                     <TabPane tab="Search by Club" key="3">
                         <ClubSearchCard />
+                        <SearchCard results={results} type={"clubs"}/>
                     </TabPane>
                 </Tabs>
             </Col>

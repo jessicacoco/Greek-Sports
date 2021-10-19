9acee9632bc6bc16744e2e4032efdab156461c38
emmaskantze <emmaskantze2018@gmail.com>
2021-10-19 14:42:07 -0400

    Updated to use only the generalized search card

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -26,15 +26,12 @@ function Home() {
             <Col span={18}>
                 <Tabs defaultActiveKey="1" centered>
                     <TabPane tab="Search by Fraternity/Sorority" key="1">
-                        <FSSearchCard />
                         <SearchCard results={results} setSearchResults={setSearchResults} type={"greek"}/>
                     </TabPane>
                     <TabPane tab="Search by Varsity/Club Sport" key="2">
-                        <SportSearchCard />
                         <SearchCard results={results} type={"sports"}/>
                     </TabPane>
                     <TabPane tab="Search by Club" key="3">
-                        <ClubSearchCard />
                         <SearchCard results={results} type={"clubs"}/>
                     </TabPane>
                 </Tabs>

79b0fb547691e4c5182cbab7fe2a622a7fbf2c2b
psaraj <90793951+psaraj@users.noreply.github.com>
2021-10-25 09:48:41 -0400

    Added logo attribute to class

--- backend/Sorority.java
+++ backend/Sorority.java
@@ -1,16 +1,19 @@
 public class Sorority {
     // each sorority class keeps track of the sorority name and that sorority's size
     String name;
+    String logo;
     int size;
 
     // class constructor
-    public Sorority(String sorority_name, int sorority_size) {
+    public Sorority(String sorority_name, String sorority_logo, int sorority_size) {
         this.name = sorority_name;
+        this.logo = sorority_logo;
         this.size = sorority_size;
     }
 
     // use public access methods to return general class info
     public String getName() { return name; }
+    public String getLogo() { return logo; }
     public int getSize() { return size; }
 }
 

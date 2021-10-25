39c7a44244a0dd891a1d1d0b0bfb6983b2d51d49
psaraj <90793951+psaraj@users.noreply.github.com>
2021-10-25 08:55:39 -0400

    Added logo attribute to class

--- backend/Frat.java
+++ backend/Frat.java
@@ -1,15 +1,18 @@
 ﻿public class Frat {
-    // each sorority class keeps track of the sorority name and that sorority's size
+    // Frat class keeps track of the Frat's name, greek letter logo, and size
     String name;
+    String logo;
     int size;
 
     // class constructor
-    public Frat(String frat_name, int frat_size) {
+    public Frat(String frat_name, String frat_logo, int frat_size) {
         this.name = frat_name;
+        this.logo = frat_logo;
         this.size = frat_size;
     }
 
     // use public access methods to return general class info
     public String getName() { return name; }
     public int getSize() { return size; }
+    public String getLogo() { return logo; }
 }

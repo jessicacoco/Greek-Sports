a15bdb4a6c342c4f08654b702e50867466cabf0c
emmaskantze <emmaskantze2018@gmail.com>
2021-10-28 01:49:03 -0400

    updated about us layout

--- frontend/src/components/pages/AboutUs.js
+++ frontend/src/components/pages/AboutUs.js
@@ -4,7 +4,7 @@ import './AboutUs.css';
 import ProfileCard from '../AboutUs/ProfileCard';
 
 function AboutUs() {
-    var paragraph = "College is stressful enough as it is, but not knowing anyone can add even more stress. Many students worry about how they will find friends for the next four years, and this application plans to solve that. Joining a fraternity or sorority can be a great way to meet people and make lots of friends for incoming students, but with so many choices, it can be stressful to choose just one. Typically, students feel comfortable around kids that play or like similar sports to them, which can be a great start to finding new friends. We built Greek Sports to allow students to do just that: find Greek life that fits them, and later, find sports and clubs that their greek organization is involved in to further connect with other members.";
+    var paragraph = "Joining a fraternity or sorority can be a great way to meet people and make lots of friends for incoming students, but with so many choices, it can be stressful to choose just one. Typically, students feel comfortable around kids that play or like similar sports to them, which can be a great start to finding new friends. We built Greek Sports to allow students to do just that: find Greek life that fits them, and later, find sports and clubs that their greek organization is involved in to further connect with other members.";
     var endb = "Backend Developer";
     var endf = "Frontend Developer";
     var year = "Class of 2022";
@@ -12,12 +12,7 @@ function AboutUs() {
         <>
             <Row>
                 <Col>
-                    <h1>Who We Are</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col span={24}>
-                    <p>{paragraph}</p>
+                    <h2>OUR CREATORS</h2>
                 </Col>
             </Row>
             <Row>
@@ -38,6 +33,16 @@ function AboutUs() {
                 </Col>
                 <Col span={4}>
                 <ProfileCard name="Chace Woods" end={endf} year={year} src="./ChaceWoods.jpg"/>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h2>OUR STORY</h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <p>{paragraph}</p>
                 </Col>
             </Row>
         </>

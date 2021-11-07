d5230209b1d79fb636d334b8abfde7c1ecf7b9b7
Jessica Coco <65690231+jessicacoco@users.noreply.github.com>
2021-11-07 16:53:28 -0500

    Delete backend directory

--- backend/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Sorority my_sorority = new Sorority(args[0], Integer.parseInt(args[1]));
-        System.out.println("Sorority: " + my_sorority.getName());
-        System.out.println("Total: " + my_sorority.getSize());
-    }
-}

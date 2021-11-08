3a4b4e1e3c430026ef969a52801b462810d24636
emmaskantze <emmaskantze2018@gmail.com>
2021-11-08 16:03:15 -0500

    More updates, created BackCard

--- frontend/src/components/Explore/FrontCard.js
+++ frontend/src/components/Explore/FrontCard.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Card } from 'antd';
-
 import './FrontCard.css';
 
+const { Meta } = Card;
 
-function FrontCard({name, topThree}) {
-    var els = [];
-    for (let i = 0; i < topThree.length; i++) {
-        els.push(<p>{topThree[i]}</p>);
-    }
+
+function FrontCard({name, letters}) {
     return (
         <>
-        <Card title={name} style={{ width: 240 }} cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
-            {els}
+        <Card bordered={true} title={letters} class="front-card">
+            {name}
         </Card>
         </>
     );
--- frontend/src/components/pages/Explore.js
+++ frontend/src/components/pages/Explore.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import FrontCard from '../Explore/FrontCard';
+import BackCard from '../Explore/BackCard';
 import './Explore.css';
 
 function Explore() {
-    let data = [['Pi Beta Phi', 'RPI Women\'s Soccer', 'Outdoors Club', 'Skiing'],
-    ['Rho Gam', 'RPI Women\'s Soccer', 'Outdoors Club', 'Skiing'],
-    ['Alpha Phi', 'RPI Women\'s Soccer', 'Outdoors Club', 'Skiing']];
+
+    let data = [['Pi Beta Phi', 'ΠΒΦ', 'RPI Women\'s Soccer', 'Outdoors Club', 'Skiing'],
+    ['Alpha Gamma Delta', 'ΑΓΔ', 'RPI Women\'s Soccer', 'Outdoors Club', 'Skiing'],
+    ['Alpha Phi', 'ΑΦ', 'RPI Women\'s Soccer', 'Outdoors Club', 'Skiing']];
 
     var cards = [];
     for (let i = 0; i < data.length; i++) {
-        cards.push(<FrontCard name={data[i][0]} topThree={data[i]}/>)
+        cards.push(
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <FrontCard name={data[i][0]} letters={data[i][1]}/>
+                </div>
+                <div class="flip-card-back">
+                    <BackCard name={data[i][0]} topThree={data[i]}/>
+                </div>
+            </div>
+        </div>);
     }
     return (
         <>

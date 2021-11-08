7cdffe6dd4609f99e75f22b2a48c6f0aae72a3bc
emmaskantze <emmaskantze2018@gmail.com>
2021-11-08 11:37:14 -0500

    Started explore page

---
+++ frontend/src/components/Explore/FrontCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card } from 'antd';
+
+function FrontCard() {
+    return (
+        <>
+        <Card style={{ width: 300 }}>
+            <p></p>
+
+        </Card>
+        </>
+    );
+}
+
+export default FrontCard;
---
+++ frontend/src/components/pages/Explore.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FrontCard from '';
+
+function Explore() {
+    return (
+        <>
+        <FrontCard />
+        </>
+    );
+}
+
+export default Explore;

453f489525589968a914be10528aaf4c466d8d04
jessicacoco <65690231+jessicacoco@users.noreply.github.com>
2021-11-19 13:04:28 -0500

    all backend tests written here

--- frontend/src/test/unit/readCSV.test.js
+++ frontend/src/test/unit/readCSV.test.js
@@ -31,6 +31,72 @@ describe('All FS Query Tests', () => {
     })
 });
 
+describe('Activity Query Tests', () => {
+    const csvData = mockReadCSVNode();
+    test('Get all Sports', () => {
+        const sportsres = activityQuery(csvData, true);
+        expect(sportsres.length).toBe(6); // there should be 6 different sports
+        // check that activity query listed all of the sports correctly
+        expect(sportsres[0]).toBe('Dance Team');
+        expect(sportsres[1]).toBe('Rugby');
+        expect(sportsres[2]).toBe('Ski Team');
+        expect(sportsres[3]).toBe('Womens Basketball');
+        expect(sportsres[4]).toBe('Womens Soccer');
+        expect(sportsres[5]).toBe('Wrestling');
+    });
+    test('Get all Clubs', () => {
+        const clubres = activityQuery(csvData, false);
+        expect(clubres.length).toBe(19); // there should be 19 different clubs
+        // check that activity query listed all of the clubs correctly
+        expect(clubres[0]).toBe('Badminton');
+        expect(clubres[1]).toBe('BallroomDance');
+        expect(clubres[2]).toBe('Boxing');
+        expect(clubres[3]).toBe('Club Volleyball');
+        expect(clubres[4]).toBe('Crew');
+        expect(clubres[5]).toBe('Dance Club');
+        expect(clubres[18]).toBe('Tae Kwon Do');
+    });
+});
+
+describe('FSQuery Tests', () => {
+    const csvData = mockReadCSVNode();
+    test('FS Query for Pi Beta Phi', () => {
+        const results = FSQuery(csvData, "Pi Beta Phi", true);
+        // there should be 10 results for activities that have Pi Beta Phi members
+        expect(results.length).toBe(10);
+    });
+    test('FS Query for Omega Phi Beta', () => {
+        const results = FSQuery(csvData, "Omega Phi Beta", true);
+        // there should be 10 results for activities that have Omega Phi Beta members
+        expect(results.length).toBe(10);
+    });
+    test('FS Query for Delta Tau Delta', () => {
+        const results = FSQuery(csvData, "Delta Tau Delta", true);
+        // there should be 20 results for activities that have Delta Tau Delta members
+        expect(results.length).toBe(20);
+    });
+});
+
+describe('Club and Sport Query Tests', () => {
+    const csvData = mockReadCSVNode();
+    test('Club and Sport Query for Racquetball', () => {
+        const results = ClubAndSportQuery(csvData, "Racquetball", true);
+        // there should be 23 results for greek organizations that have members in Racuqetball club
+        expect(results.length).toBe(23);
+    });
+    test('Club and Sport Query for Womens Soccer', () => {
+        const results = ClubAndSportQuery(csvData, "Womens Soccer", true);
+        // there should be 6 results for greek organizations that have members in Womens Soccer
+        expect(results.length).toBe(6);
+    });
+    test('Club and Sport Query for Boxing', () => {
+        const results = ClubAndSportQuery(csvData, "Boxing", true);
+        // there should be 26 results for greek organizations that have members in Boxing
+        expect(results.length).toBe(26);
+    });
+});
+
+
 describe('FSQueryTopThree Tests', () => {
     const csvData = mockReadCSVNode();
     test('Top Three for AGD', () => {

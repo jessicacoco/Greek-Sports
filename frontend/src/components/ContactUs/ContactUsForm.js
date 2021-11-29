fdb784a4445ac086ecdc44abccd21dc18037323f
emmaskantze <emmaskantze2018@gmail.com>
2021-11-29 17:09:08 -0500

    updated contact us form

--- frontend/src/components/ContactUs/ContactUsForm.js
+++ frontend/src/components/ContactUs/ContactUsForm.js
@@ -1,5 +1,8 @@
 import React, { useRef } from 'react';
+
+import { message } from 'antd';
 import emailjs from 'emailjs-com';
+
 import './ContactUsForm.css';
 
 // Creates ContactUs form for ContactUs page.
@@ -11,6 +14,7 @@ export const ContactUsForm = () => {
     
     /* On form submission, if all required fields are filled, send user a Success Message.
     Send email to developers' email. */
+    message.success('Message sent');
     emailjs.sendForm('gmail', 'test_template', form.current, 'user_ndZKEDaxn9hHCXBedYIaH')
       .then((result) => {
           console.log(result.text);
@@ -22,9 +26,9 @@ export const ContactUsForm = () => {
 
   return (
     <form ref={form} onSubmit={sendEmail}>
-      <input type="text" name="from_name" placeholder="Name"/><br/><br/>
-      <input type="email" name="user_email" placeholder="Email"/><br/><br/>
-      <textarea name="message" placeholder="Message"/><br/><br/>
+      <input type="text" name="from_name" placeholder="Name" required/><br/><br/>
+      <input type="email" name="user_email" placeholder="Email" required/><br/><br/>
+      <textarea name="message" placeholder="Message" required/><br/><br/>
       <input type="submit" value="Send" />
     </form>
   );
